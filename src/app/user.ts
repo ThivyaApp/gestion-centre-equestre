6c60e35b04fa263757ea8878187d5e060f499b17
ThivyaApp <appudurai@et.esiea.fr>
2020-10-25 13:34:20 +0100

    user interface created

---
+++ src/app/user.ts
@@ -0,0 +1,11 @@
+export interface User {
+    id: number;
+    name: string;
+    email: string;
+    password: string;
+    firstname : string;
+    secondname: string;
+    num: string ;
+    licensenum: string;
+    usertype: string;
+}
