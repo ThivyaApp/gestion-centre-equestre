b25799541ae62c395a5e010c0f72fff84fc4f9e6
ThivyaApp <appudurai@et.esiea.fr>
2020-11-14 16:35:33 +0100

    merge without conflit updated2

--- src/app/mock-users.ts
+++ src/app/mock-users.ts
@@ -2,14 +2,14 @@ import { bindCallback } from 'rxjs';
 import { User } from './user';
 
 export const USERS: User[] = [
-  { id: 1, email: 'brice@gmail.com', password: 'passytest', firstname: 'Brice', lastname: 'bg', num: '0660675951', licensenum: '10012358', usertype: 'admin' },
-  { id: 2, email: 'thivya@gmail.com', password: 'passytest',firstname: 'Thivya', lastname: 'moche', num: '0660678551', licensenum: '10012359', usertype: 'admin' },
-  { id: 3, email: 'douglas@gmail.com', password: 'passytest',firstname: 'Douglas', lastname: 'Douggy', num: '0660675585', licensenum: '10012353', usertype: 'admin' },
-  { id: 4, email: 'sahani@gmail.com', password: 'passytest',firstname: 'Sahani', lastname: 'la boga', num: '0660675714', licensenum: '10012355', usertype: 'user' },
-  { id: 5, email: 'rokia@gmail.com', password: 'passytest', firstname: 'Rokia', lastname: 'couli', num: '0660669881', licensenum: '10012356', usertype: 'user' },
-  { id: 1, email: 'brice@gmail.com', password: 'passytest', firstname: 'Brice', lastname: 'bg', num: '0660675951', licensenum: '10012358', usertype: 'admin' },
-  { id: 2, email: 'thivya@gmail.com', password: 'passytest',firstname: 'Thivya', lastname: 'moche', num: '0660678551', licensenum: '10012359', usertype: 'admin' },
-  { id: 3, email: 'douglas@gmail.com', password: 'passytest',firstname: 'Douglas', lastname: 'Douggy', num: '0660675585', licensenum: '10012353', usertype: 'admin' },
-  { id: 4, email: 'sahani@gmail.com', password: 'passytest',firstname: 'Sahani', lastname: 'la boga', num: '0660675714', licensenum: '10012355', usertype: 'user' },
-  { id: 5, email: 'rokia@gmail.com', password: 'passytest', firstname: 'Rokia', lastname: 'couli', num: '0660669881', licensenum: '10012356', usertype: 'user' }
+  { id: 1, username: 'fakeusername', email: 'brice@gmail.com', password: 'passytest', firstname: 'Brice', lastname: 'bg', num: '0660675951', licensenum: '10012358', usertype: 'admin' },
+  { id: 2, username: 'fakeusername', email: 'thivya@gmail.com', password: 'passytest',firstname: 'Thivya', lastname: 'moche', num: '0660678551', licensenum: '10012359', usertype: 'admin' },
+  { id: 3, username: 'fakeusername',  email: 'douglas@gmail.com', password: 'passytest',firstname: 'Douglas', lastname: 'Douggy', num: '0660675585', licensenum: '10012353', usertype: 'admin' },
+  { id: 4, username: 'fakeusername', email: 'sahani@gmail.com', password: 'passytest',firstname: 'Sahani', lastname: 'la boga', num: '0660675714', licensenum: '10012355', usertype: 'user' },
+  { id: 5, username: 'fakeusername', email: 'rokia@gmail.com', password: 'passytest', firstname: 'Rokia', lastname: 'couli', num: '0660669881', licensenum: '10012356', usertype: 'user' },
+  { id: 1, username: 'fakeusername', email: 'brice@gmail.com', password: 'passytest', firstname: 'Brice', lastname: 'bg', num: '0660675951', licensenum: '10012358', usertype: 'admin' },
+  { id: 2, username: 'fakeusername', email: 'thivya@gmail.com', password: 'passytest',firstname: 'Thivya', lastname: 'moche', num: '0660678551', licensenum: '10012359', usertype: 'admin' },
+  { id: 3, username: 'fakeusername', email: 'douglas@gmail.com', password: 'passytest',firstname: 'Douglas', lastname: 'Douggy', num: '0660675585', licensenum: '10012353', usertype: 'admin' },
+  { id: 4, username: 'fakeusername', email: 'sahani@gmail.com', password: 'passytest',firstname: 'Sahani', lastname: 'la boga', num: '0660675714', licensenum: '10012355', usertype: 'user' },
+  { id: 5, username: 'fakeusername', email: 'rokia@gmail.com', password: 'passytest', firstname: 'Rokia', lastname: 'couli', num: '0660669881', licensenum: '10012356', usertype: 'user' }
 ];
